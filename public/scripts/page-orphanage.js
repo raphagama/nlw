da17e37f2aefd6deb7755355f366e346983a5332
Raphael Gama <raphaelgama19@gmail.com>
2020-10-17 00:38:15 -0300

    Outras

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,52 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-1.3606198,-48.4100636], 16);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon =  L.icon({
+    iconUrl:"./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L
+.marker([-1.3606198,-48.4100636], { icon })
+.addTo(map)
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as clases active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+        button.classList.remove(".active")
+    }
+
+
+    //selcionar a imagem clicada 
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container de imagem
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add('active')
+}
